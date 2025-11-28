385eba18b6d89c1ed4b7dc62413329682cfd8dbd
rahma-zulla <rahmazullazulla@gmail.com>
2025-11-28 09:08:53 +0700

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,94 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+
+  <footer class="text-white text-center text-lg-start bg-dark">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row mt-4">
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4">Pretty Laundry</h5>
+
+          <p>
+            Pretty Laundry - Pretty laundry milik seorang wanita cantik yg suka mencuci pakaian. jadi jika anda mencucikan pakaian di pretty laundry dijamin bersih, wangi, dan rapi 👍🏻. Di Pretty Laundry ini menyediakan layanan cuci kering, cuci setrika, laundry satuan, laundry kiloan, dan antar jemput. di Pretty Laundry selain mencucikan pakaian, disini jg tersedia mencuci untuk sprei, selimut, bahan kain yg berat.
+          </p>
+
+          <p>
+            Alamat = jl.Mawar Coqquete No.23 Kota Yogyakarta (DIY) 
+          </p>
+
+          <div class="mt-4">
+            <!-- Facebook -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-facebook-f"></i></a>
+            <!-- Dribbble -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-dribbble"></i></a>
+            <!-- Twitter -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-twitter"></i></a>
+            <!-- Google + -->
+            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-google-plus-g"></i></a>
+            <!-- Linkedin -->
+          </div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4 pb-1">Search Something</h5>
+
+          <div class="form-outline form-white mb-4">
+            <input type="text" id="formControlLg" class="form-control form-control-lg" />
+            <label class="form-label" for="formControlLg">Search</label>
+          </div>
+
+          <ul class="fa-ul" style="margin-left: 1.65em;">
+            <li class="mb-3">
+              <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">Jennie, 1101, Indonesia</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">prettyjennie@gmail.com</span>
+            </li>
+            <li class="mb-3">
+              <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+ 23 1207 110 19</span>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-4">Jam Buka</h5>
+
+          <table class="table text-center text-white">
+            <tbody class="fw-normal">
+              <tr>
+                <td>Selasa - Rabu:</td>
+                <td>09.00 - 20.00</td>
+              </tr>
+              <tr>
+                <td>kamis - minggu:</td>
+                <td>09.00 - 19.00</td>
+              </tr>
+              <tr>
+                <td>Senin:</td>
+                <td>--libur--</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      
+      
+    </div>
+    <!-- Copyright -->
+  </footer>
+
+</div>
+<!-- End of .container -->
---
+++ admin/transaksi_update.php
@@ -0,0 +1,40 @@
+<?php
+// menghubungkan dengan koneksi
+include '../koneksi.php';
+
+// menangkap data yang dikirim dari form
+$id = $_POST['id'];
+$pelanggan = $POST['pelanggan'];
+$berat = $_POST['berat'];
+$tgl_selesai = $_POST['tgl_selesai'];
+
+$status = $_POST['status'];
+
+//mengambil data harga per kilo dari database
+$h = mysqli_query($koneksi,"select harga_per_kilo from harga");
+$harga_per_kilo = mysqli_fetch_assoc($h);
+
+// menghitung harga laundey, harga perkilo x berat cucian
+$harga = $berat * $harga_per_kilo['harga_per_kilo'];
+
+
+// update data transaksi
+mysqli_query(koneksi,"update transaksi set transaksi_pelanggan='$pelanggan', transaksasi_harga='$harga', transaksi_berat='$berat',transaksi_tgl_selesai='$tgl_selesai', transaksi_status='$status' where transaksi_id='$id'");
+
+
+// menangkap data form input array (jenis pakaian dan jumlah pakaian)
+$jenis_pakaian = $_POST['jenis_pakaian'];
+$jumlah_pakaian = $_POST['jumlah_pakaian'];
+
+// menghapus semua pakaian dalam transaksi ini
+mysqli_query($koneksi,"detele from pakaian where pakaian_transaksi='$id'");
+
+// input ulang data cucian berdasarkan id transaksi (invoice) ke table pakaian
+for($x=0;$x<count($jenis_pakaian);$x++){
+	if($jenis_pakaian[$x] != ""){
+		mysqli_query($koneksi,"insert into pakaian values('','$id','$jenis_pakaian[$x]','jumlah_pakaian[$x]')");
+
+	}
+}
+header("location:transaksi.php");
+?>
